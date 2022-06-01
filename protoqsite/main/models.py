a82b7b55f8391b323f23c052181f4b013a485a5d
James Dietz <jamesthespeedy@gmail.com>
2022-06-01 19:12:55 -0400

    a

--- protoqsite/main/models.py
+++ protoqsite/main/models.py
@@ -1,15 +1,12 @@
-
-from ctypes import BigEndianStructure, addressof
-from tkinter.filedialog import asksaveasfile
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.forms import IntegerField
 
-
-LAYER_HEIGHTS = ["0.1mm", "0.15mm", "0.2mm", "0.25mm", "0.3mm", "0.35mm", "0.4mm", "0.45mm", "0.5mm", "0.55mm", "0.6mm"]
-NOZZLE_SIZES = ["0.25", "0.4", "0.8"]
-MATERIAL_LIST = []
-INFILL_TYPES = []
-INFILL_DENSITIES = []
+LAYER_HEIGHTS = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]
+NOZZLE_SIZES = [0.25, 0.4, 0.8]
+MATERIAL_LIST = ["PLA", "PETG"]
+INFILL_TYPES = ["Grid"]
+INFILL_DENSITIES = [range(0,101,1)]
+ORDER_STATUS_OPTS = ["Awaiting shipping", "awaiting confirm..."]
 
 
 class Print(models.Model):
@@ -50,18 +47,26 @@ class Print(models.Model):
 
 class Order(models.Model):
 
-    order_id models.AutoField(primary_key=True)D
-    print_id = 
-    order status
-    buyer_id = 
-    seller_id = 
-    seller_address:
+    order_id = models.AutoField(primary_key=True)
+    order_price = models.DecimalField(
+        max_digits=9,
+        decimal_places=2,
+    )
+    order_qty = models.IntegerField(max_length=1000, default=1)
+    print_id = models.IntegerField()
+    order_status = models.CharField(max_length=20, choices=ORDER_STATUS_OPTS)
+    buyer_id = models.IntegerField()
+    seller_id = models.IntegerField()
+    """seller_address:
     buyer_address:
     buyer payment information
-    seller payment information
-    shipping tracking
-    confirmation photos
-    buyer photo approval
+    seller payment information"""
+    shipping_tracking = models.CharField(max_length=100)
+    confirmation_photos = models.URLField()
+    confirm_approval = models.BooleanField()
+
+
+make a new class for purchases after the order is closed
 
 class Buyer(models.Model):
     buyer_id= models.AutoField(primary_key=True)
@@ -80,22 +85,3 @@ class Seller(models.Model):
     open asks
     closed asks
     feedback
-
-
-
-
-    class nozzleSizes(models.TextChoices):
-        SML = "0.25mm"
-        MED = "0.4mm"
-        LRG = "0.8mm"
-    
-    nozzle_size  = models.TextField(
-        choices = NOZZLE_SIZES,
-        default = nozzleSizes.MED
-    )
-
-    modelLink = models.URLField()
-    modelName = models.TextField()
-    modelLink = models.TextField()
-
-    printHeight = models.TextChoices(LAYER_HEIGHTS)
